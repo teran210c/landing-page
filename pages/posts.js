17be1667408712fd3883ae2d009c39c5be60ced4
teran210c <you@example.com>
2025-01-17 12:17:18 -0700

    cargar button is working

--- pages/posts.js
+++ pages/posts.js
@@ -4,23 +4,30 @@ import { useState, useEffect } from "react";
 export default function Post () {
     const [posts, setPost] = useState();
 
-    useEffect(() => {
-       const updataData = async () => {
+    // useEffect(() => {
+       
+    //    updataData();
+    // }, []);
+
+    const updataData = async () => {
         const response = await axios.get("https://jsonplaceholder.typicode.com/posts");
         console.log(response.data);
         setPost(response.data);
        };
-       updataData();
-    }, []);
+
     const resetData = () => {
         setPost([]);
     };
+
     return (
         <div>
             <h1>Posts</h1>
             <button
                 onClick={()=>resetData()}
             >Reset</button>
+            <button
+                onClick={()=>updataData()}
+            >Cargar</button>
             {posts && posts?.length >0 && posts?.map( (post) => (                
                         <div key={post.id}>
                             <h2>{post.title}</h2>
